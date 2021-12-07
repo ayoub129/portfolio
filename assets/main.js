e77b661785d26a637af7608c630df3233dda10e9
ayoub129 <ayoub.berouijel10@gmail.com>
2021-12-07 12:13:20 +0100

    static portfolio

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+const date = document.getElementById("date")
+const opening = document.getElementById("open")
+const navigation = document.getElementById("nav-phone")
+
+date.textContent = new Date().getFullYear()
+
+    opening.addEventListener('click' , () => {
+        navigation.classList.toggle('block')
+    })
