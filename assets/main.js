b400631c4e19eb98406141ed0755686386c30f1e
ayoub129 <ayoub.berouijel10@gmail.com>
2021-12-10 14:36:16 +0100

    portfolio editing

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,7 @@ const date = document.getElementById("date")
 const opening = document.getElementById("open")
 const navigation = document.getElementById("nav-phone")
 
+
 date.textContent = new Date().getFullYear()
 
     opening.addEventListener('click' , () => {
