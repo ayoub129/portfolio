cfffdb764d39d48be1a89df9756dbf04f1480d2f
ayoub129 <ayoub.berouijel10@gmail.com>
2021-12-15 11:31:32 +0100

    updates

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,40 @@
+// variables
 const date = document.getElementById("date")
 const opening = document.getElementById("open")
 const navigation = document.getElementById("nav-phone")
 
+// animation
+AOS.init();
 
+// date
 date.textContent = new Date().getFullYear()
 
+// hamburger menu
     opening.addEventListener('click' , () => {
         navigation.classList.toggle('block')
     })
+
+    // slider
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+
+    function currentSlide(n) {
+    showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    }
