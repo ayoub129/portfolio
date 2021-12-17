4ad7c3ec50c2de69420b8db3f3d16fbe3bf5cccd
ayoub129 <ayoub.berouijel10@gmail.com>
2021-12-17 21:44:02 +0100

    fix projects and services pages

--- assets/main.js
+++ assets/main.js
@@ -1,59 +1,38 @@
-// variables
-const date = document.getElementById("date");
-const opening = document.getElementById("open");
-const navigation = document.getElementById("nav-phone");
-
-// animation
-AOS.init();
-
-// date
-date.textContent = new Date().getFullYear();
-
-// hamburger menu
-opening.addEventListener("click", () => {
-  navigation.classList.toggle("block");
-});
-
-// slider
+const date = document.getElementById("date"),
+  opening = document.getElementById("open"),
+  navigation = document.getElementById("nav-phone");
+AOS.init(),
+  (date.textContent = new Date().getFullYear()),
+  opening.addEventListener("click", () => {
+    navigation.classList.toggle("block");
+  });
 let slideIndex = 1;
-showSlides(slideIndex);
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
+function currentSlide(e) {
+  showSlides((slideIndex = e));
 }
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+function showSlides(e) {
+  let n,
+    t = document.getElementsByClassName("mySlides"),
+    s = document.getElementsByClassName("dot");
+  for (
+    e > t.length && (slideIndex = 1), e < 1 && (slideIndex = t.length), n = 0;
+    n < t.length;
+    n++
+  )
+    t[n].style.display = "none";
+  for (n = 0; n < s.length; n++)
+    s[n].className = s[n].className.replace(" active", "");
+  (t[slideIndex - 1].style.display = "block"),
+    (s[slideIndex - 1].className += " active");
 }
-
-// sticky navbar
-window.onscroll = function () {
-  myFunction();
-};
-
-let navbar = document.getElementById("navbar");
-let sticky = navbar.offsetTop;
-
+showSlides(slideIndex),
+  (window.onscroll = function () {
+    myFunction();
+  });
+let navbar = document.getElementById("navbar"),
+  sticky = navbar.offsetTop;
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
+  window.pageYOffset >= sticky
+    ? navbar.classList.add("sticky")
+    : navbar.classList.remove("sticky");
 }
