b4c003c725f9fc16f81a092aa8fb2914be4662eb
ayoub129 <ayoub.berouijel10@gmail.com>
2021-12-17 21:28:05 +0100

    fix projects and services pages

--- assets/main.js
+++ assets/main.js
@@ -1,40 +1,59 @@
 // variables
-const date = document.getElementById("date")
-const opening = document.getElementById("open")
-const navigation = document.getElementById("nav-phone")
+const date = document.getElementById("date");
+const opening = document.getElementById("open");
+const navigation = document.getElementById("nav-phone");
 
 // animation
 AOS.init();
 
 // date
-date.textContent = new Date().getFullYear()
+date.textContent = new Date().getFullYear();
 
 // hamburger menu
-    opening.addEventListener('click' , () => {
-        navigation.classList.toggle('block')
-    })
+opening.addEventListener("click", () => {
+  navigation.classList.toggle("block");
+});
 
-    // slider
-    let slideIndex = 1;
-    showSlides(slideIndex);
+// slider
+let slideIndex = 1;
+showSlides(slideIndex);
 
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
 
-    function currentSlide(n) {
-    showSlides(slideIndex = n);
-    }
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
 
-    function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    }
+// sticky navbar
+window.onscroll = function () {
+  myFunction();
+};
+
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
